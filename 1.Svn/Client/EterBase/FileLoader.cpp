3987efc44690cedad6c3d044e18f7ff2e0201a54
example <33325253+blackdragonx61@users.noreply.github.com>
2022-10-30 19:14:54 +0300

    Create FileLoader.cpp
    Fixing some file load problems.
    
    This fix comes from official binary too, not my custom fix.

---
+++ 1.Svn/Client/EterBase/FileLoader.cpp
@@ -0,0 +1,13 @@
+//Find in void CMemoryTextFileLoader::Bind(int bufSize, const void* c_pvBuf)
+		else if (c < 0)
+		{
+			stLine.append(c_pcBuf + (pos-1), 2);
+			++pos;
+		}
+
+///Change
+		/*else if (c < 0)
+		{
+			stLine.append(c_pcBuf + (pos-1), 2);
+			++pos;
+		}*/
